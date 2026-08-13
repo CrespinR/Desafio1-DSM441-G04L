bf0b9659ffddb13788b8b7c0cf28dd5b024ba7f6
CrespinR <bryanpppe@gmail.com>
2026-08-12 22:02:04 -0600

    conectar menu con ejercicios y configuracion de recursos

--- app/src/main/java/com/example/desafio1dsm_lab/MainActivity.kt
+++ app/src/main/java/com/example/desafio1dsm_lab/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.desafio1dsm_lab
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnPromedioEstudiante =
+            findViewById<Button>(R.id.btnPromedioEstudiante)
+
+        val btnDescuentosSalario =
+            findViewById<Button>(R.id.btnDescuentosSalario)
+
+        val btnCalculadoraBasica =
+            findViewById<Button>(R.id.btnCalculadoraBasica)
+
+        btnPromedioEstudiante.setOnClickListener {
+            val intent = Intent(this, PromedioActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnDescuentosSalario.setOnClickListener {
+            val intent = Intent(this, SalarioActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCalculadoraBasica.setOnClickListener {
+            val intent = Intent(this, CalculadoraActivity::class.java)
+            startActivity(intent)
         }
     }
+
 }
